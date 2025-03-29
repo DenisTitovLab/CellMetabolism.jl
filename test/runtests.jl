cf3cc99c8cb33741bc290b2fc0efa7b2163954c4
Denis-Titov <titov@berkeley.edu>
2025-03-28 23:44:26 -0700

    add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,14 +1,3 @@
-using CellMetabolism
-using Test
-using Aqua
-using JET
+using TestItemRunner
 
-@testset "CellMetabolism.jl" begin
-    @testset "Code quality (Aqua.jl)" begin
-        Aqua.test_all(CellMetabolism)
-    end
-    @testset "Code linting (JET.jl)" begin
-        JET.test_package(CellMetabolism; target_defined_modules = true)
-    end
-    # Write your tests here.
-end
+@run_package_tests

8941a9433e04c2c945b7ae2182eedd0805845023
Denis-Titov <titov@berkeley.edu>
2025-10-17 16:58:52 -0700

    bump compat to CellMetabolismBase 0.3.1 and uncomment tests

--- test/tests_validate_pathways.jl
+++ test/tests_validate_pathways.jl
@@ -1,11 +1,9 @@
-# Skip this test while CellMetabolismBase 0.3 is being updated to handle regulators that are also substrates/products
+@testitem "MetabolicPathways Validation Tests" begin
+    using LabelledArrays, BenchmarkTools, OrdinaryDiffEq, CellMetabolismBase
 
-# @testitem "MetabolicPathways Validation Tests" begin
-#     using LabelledArrays, BenchmarkTools, OrdinaryDiffEq, CellMetabolismBase
-
-#     @test_nowarn CellMetabolismBase.validate_MetabolicPathway(
-#         CellMetabolism.glycolysis_pathway,
-#         CellMetabolism.glycolysis_init_conc,
-#         CellMetabolism.glycolysis_params,
-#     )
-# end
+    @test_nowarn CellMetabolismBase.validate_MetabolicPathway(
+        CellMetabolism.glycolysis_pathway,
+        CellMetabolism.glycolysis_init_conc,
+        CellMetabolism.glycolysis_params,
+    )
+end

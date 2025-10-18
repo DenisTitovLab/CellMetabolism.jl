ff9878239c28dc31485353456140bdf224513950
Denis-Titov <titov@berkeley.edu>
2025-10-17 17:26:50 -0700

    change validate_MetabolicPathway to validate

--- test/tests_validate_pathways.jl
+++ test/tests_validate_pathways.jl
@@ -1,7 +1,7 @@
 @testitem "MetabolicPathways Validation Tests" begin
     using LabelledArrays, BenchmarkTools, OrdinaryDiffEq, CellMetabolismBase
 
-    @test_nowarn CellMetabolismBase.validate_MetabolicPathway(
+    @test_nowarn CellMetabolismBase.validate(
         CellMetabolism.glycolysis_pathway,
         CellMetabolism.glycolysis_init_conc,
         CellMetabolism.glycolysis_params,
